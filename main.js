cea441d55f41914c80c0112ae7d3f3c2716690dc
Cy <cy.f.harwood@gmail.com>
2022-07-03 16:51:30 -0400

    final random quote functionality

--- main.js
+++ main.js
@@ -13,36 +13,42 @@ function newQuote(quote, citation) {
 //create objects of quotes & citations, put quotes in array
 let quote1 = newQuote("This town is a part of us all... a part of us all... a part of us all", 
 "Marge, Lemon of Troy (S06E24)");
-quoteArray.push(quote1.quote);
+quoteArray.push(quote1);
 
-let quote2 = newQuote("When I grow up, I'm going to Bovine University!", "Ralph Wiggum Lisa the Vegetarian (S07E05");
-quoteArray.push(quote2.quote);
+let quote2 = newQuote("When I grow up, I'm going to Bovine University!", "Ralph Wiggum, Lisa the Vegetarian (S07E05)");
+quoteArray.push(quote2);
 
-let quote3 = newQuote("It takes two to lie; one to lie, and one to listen.", "Homer, Colonel Homer (S03E20");
-quoteArray.push(quote3.quote);
+let quote3 = newQuote("It takes two to lie; one to lie, and one to listen.", "Homer, Colonel Homer (S03E20)");
+quoteArray.push(quote3);
 
 
-//function to generate random quote
 
-function getRandomQuote() {
-    let randNum = Math.floor(Math.random()*3);
-    let randQuote = quoteArray[randNum];
+let randNum = Math.floor(Math.random()*quoteArray.length);
+
+function getQuote() {
+    let randQuote = quoteArray[randNum].quote;
     return randQuote;
-};
+}
 
-console.log(getRandomQuote());
+function getCitation() {
+    let randCitation = quoteArray[randNum].citation;
+    return randCitation;
+}
+
+console.log(getQuote());
+console.log(getCitation());
 
 
 let simpsonQuote = document.getElementById("quote-quote");
 let quoteInfo = document.getElementById("quote-citation");
 
 function updateQuote() {
-    simpsonQuote.innerHTML = getRandomQuote();
-    quoteInfo.innerHTML = 'citation info';
+    randNum = Math.floor(Math.random()*quoteArray.length);
+    simpsonQuote.innerHTML = getQuote();
+    quoteInfo.innerHTML = getCitation();
 }
 
 let quoteButton = document.getElementById("quote-button");
 
 //quoteButton.addEventListener('click', updateQuote);
 quoteButton.onclick = updateQuote;
-
